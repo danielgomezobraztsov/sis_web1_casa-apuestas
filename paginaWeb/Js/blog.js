95878031f7f6249e56e9d0d865e31f77cd160e95
GiulioTizzano <giuliofrancesco.tizzano@usp.ceu.es>
2025-11-03 20:33:19 +0100

    blog.html /.js /.css

---
+++ paginaWeb/Js/blog.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const logMenuRoute = "logMenu.html";
+  const goTo = (route) => (window.location.href = route);
+
+  const signupBtn = document.querySelector('[data-action="signup"]');
+  const loginBtn  = document.querySelector('[data-action="login"]');
+
+  if (signupBtn) signupBtn.addEventListener("click", () => goTo(logMenuRoute));
+  if (loginBtn)  loginBtn.addEventListener("click", () => goTo(logMenuRoute));
+
+  document.querySelectorAll("[data-readmore]").forEach((a) => {
+    a.addEventListener("click", (e) => {
+      e.preventDefault();
+      alert("Read More: coming soon.");
+    });
+  });
+});
