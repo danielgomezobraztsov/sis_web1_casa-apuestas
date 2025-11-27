b7f4535ae373eaed17a256a3a945c0a712ee01fe
Alejandro Fernandez <alejandrofemu@gmail.com>
2025-11-27 22:45:18 +0100

    añadidas las rutas en app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,6 +3,17 @@ import express from "express";
 import cors from "cors";
 import path from "path";
 import { fileURLToPath } from "url";
+const logger = require("morgan");
+
+const indexRouter = require("./routes/index");
+const accountRouter = require("./routes/account");
+const blogRouter = require("./routes/blog");
+const gabmletRouter = require("./routes/gamble");
+const helpRouter = require("./routes/help");
+const logMenuRouter = require("./routes/logMenu");
+const resultRouter = require("./routes/result");
+const settingsRouter = require("./routes/settings");
+const subscriptionRouter = require("./routes/subscription");
 
 const app = express();
 
@@ -17,10 +28,16 @@ app.use(express.json());
 // Servir tu frontend desde /public
 app.use(express.static(path.join(__dirname, "../public")));
 
-// Ruta principal (por si entras a /)
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "../public/html/index.html"));
-});
+//Actiivar las rutas
+app.use("/", indexRouter);
+app.use("/account", accountRouter);
+app.use("/blog", blogRouter);
+app.use("/gamble", gabmletRouter);
+app.use("/help", helpRouter);
+app.use("/logMenu", logMenuRouter);
+app.use("/result", resultRouter);
+app.use("/settings", settingsRouter);
+app.use("/subscription", subscriptionRouter);
 
 // Exportar app correctamente (esto soluciona el error)
 export default app;
