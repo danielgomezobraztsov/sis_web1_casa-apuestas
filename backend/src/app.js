50b86a1658eaef6dc32b0924dae26517759496e6
GiulioTizzano <giuliofrancesco.tizzano@usp.ceu.es>
2025-11-27 23:32:56 +0100

    Progreso
    Hemos creado las rutas, las vistas y ya cargan el HTML. Ahora hace falta que se cargue el JS y el CSS del frontend mediante el servidor tmb.

--- backend/src/app.js
+++ backend/src/app.js
@@ -18,9 +18,13 @@ import subscriptionRouter from "./routes/subscription.js";
 
 const app = express();
 
+
 // Necesario para rutas absolutas
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 // Middlewares
 app.use(cors());
