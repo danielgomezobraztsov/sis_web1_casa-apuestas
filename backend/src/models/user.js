88fa7553f5eae4295f85d61dc4afaac45c34dd7b
Alejandro Fernandez <alejandrofemu@gmail.com>
2025-11-29 11:58:17 +0100

    modelo de usuario para bbdd

---
+++ backend/src/models/user.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true, // Asegura que no haya usuarios con el mismo nombre
+        trim: true, // Elimina espacios en blanco al inicio y al final
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    balance: { // Saldo del usuario para apuestas
+        type: Number,
+        default: 0,
+    }
+}, { timestamps: true }); // timestamps agrega createdAt y updatedAt automáticamente
+
+export default mongoose.model("User", userSchema);
